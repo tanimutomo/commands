f867d4261a9e62da3f14913799073a4b71b01dc1
Tomoki Tanimura <tanimutomo@gmail.com>
2026-04-03 15:28:27 +0900

    update

---
+++ raycast/aws-sso-login.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title aws sso login
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author tanimutomo
+# @raycast.authorURL https://raycast.com/tanimutomo
+
+# Warpでコマンドを実行する魔法の1行
+open "warp://action/new_tab?path=$HOME&command=aws%20sso%20login%20--profile%20tanimutomo"
+
